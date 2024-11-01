89fe21832ddf1e44cc003331495c665fef1915e1
Dmitry Ponyatov <dponyatov@gmail.com>
2024-11-01 23:59:23 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- src/jc.ml
+++ src/jc.ml
@@ -45,6 +45,9 @@ void arg(int argc, char *argv);
   "
 int main(int argc, char *argv[]) {  //
     arg(0, argv[0]);
+    for (int i = 1; i < argc; i++) {  //
+        arg(i, argv[i]);
+    }
 }
 
 void arg(int argc, char *argv) {  //
